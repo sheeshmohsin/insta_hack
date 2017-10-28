708b067c49c92fc73c53cba24eb11b828dbca9f4
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-10-28 20:33:14 +0530

    Added pan card verification codes

--- panverification/panapp/tasks.py
+++ panverification/panapp/tasks.py
@@ -2,14 +2,16 @@ from __future__ import absolute_import, unicode_literals
 import requests
 from celery import task
 from panapp.models import UserData, FailedUserData
-from panapp.utils import extract_text, check_if_pan_card_pic
-from panapp.constants import IMG_INVALID
+from panapp.utils import extract_text, check_if_pan_card_pic, \
+			detect_if_not_forged, match_with_user_data
+from panapp.constants import IMG_INVALID, IMG_FORGED
 from django.conf import settings
 
 @task()
 def image_validation():
 	unscanned_data = UserData.objects.filter(is_scanned=False)
 	for data in unscanned_data:
+		# data.is_scanned = True
 		r = extract_text(data)
 		if r.status_code != 200:
 			print "continue due to status_code != 200"
@@ -28,6 +30,21 @@ def image_validation():
 		if not check_if_pan_card_pic(parsed_text):
 			FailedUserData.objects.create(user_data=data, invalid=IMG_INVALID)
 			data.is_invalid_auto = True
+			data.save()
+			continue
+
+		if not detect_if_not_forged(data.image):
+			FailedUserData.objects.create(user_data=data, invalid=IMG_FORGED)
+			data.is_invalid_auto = True
+			data.save()
+			continue
+
+		res = match_with_user_data(parsed_text, data)
+		if not res['status']:
+			for reason in res['reason']:
+				FailedUserData.objects.create(user_data=data, invalid=reason)
+			data.is_invalid_auto = True
+			data.save()
 			continue
 
 
--- panverification/panapp/utils.py
+++ panverification/panapp/utils.py
@@ -1,5 +1,6 @@
 import uuid, os, requests
 from django.conf import settings
+from panapp.constants import NAME_INVALID, DOB_INVALID, PAN_INVALID
 
 def get_upload_file_path(instance, filename):
     ext = filename.split('.')[-1]
@@ -19,6 +20,11 @@ def extract_text(data):
 
 def check_if_pan_card_pic(parsed_text):
 	status = False
+	if len(parsed_text) > 6:
+		if 'INCOME TAX DEPARTMENT' in str(parsed_text[0]).strip().upper():
+			if 'GOVT. OF INDIA' in str(parsed_text[5]).strip().upper():
+				status = True
+				return status
 	for text in parsed_text:
 		if 'INCOME TAX DEPARTMENT' in text.strip():
 			status = True
@@ -26,4 +32,42 @@ def check_if_pan_card_pic(parsed_text):
 			status = True
 	return status
 
+
+def detect_if_not_forged(img_url):
+	return True
+
+
+def match_with_user_data(parsed_text, data):
+	name_matched = False
+	dob_matched = False
+	pan_matched = False
+	res = {'status': False, 'reason': []}
+	if len(parsed_text) > 6:
+		if str(parsed_text[1]).strip().upper() == str(data.name).strip().upper():
+			name_matched = True
+		if str(parsed_text[3]).strip().upper() == str(data.dob).strip().upper():
+			dob_matched = True
+		if str(parsed_text[5]).strip().upper() == str(data.pan).strip().upper():
+			pan_matched = True
+	if name_matched and dob_matched and pan_matched:
+		res['status'] = True
+		return res
+	for text in parsed_text:
+		if str(text).strip().upper() == str(data.name).strip().upper():
+			name_matched = True
+		if str(text).strip().upper() == str(data.dob).strip().upper():
+			dob_matched = True
+		if str(text).strip().upper() == str(data.pan).strip().upper():
+			pan_matched = True
+	if name_matched and dob_matched and pan_matched:
+		res['status'] = True
+		return res
+	res['status'] = False
+	if not name_matched:
+		res['reason'].append(NAME_INVALID)
+	if not dob_matched:
+		res['reason'].append(DOB_INVALID)
+	if not pan_matched:
+		res['reason'].append(PAN_INVALID)
+	return res
 	
