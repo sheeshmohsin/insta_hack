c40aa7d40b956d1d9ee218babf9fcc67f48a3a2b
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-10-28 16:31:03 +0530

    Added constants

---
+++ panverification/panapp/constants.py
@@ -0,0 +1,26 @@
+
+NAME_INVALID = 1
+DOB_INVALID = 2
+PAN_INVALID = 3
+IMG_INVALID = 4
+IMG_FORGED = 5
+
+INVALID_CHOICES = (
+	(NAME_INVALID, "Name Invalid"),
+	(DOB_INVALID, "DOB Invalid"),
+	(PAN_INVALID, "PAN Invalid"),
+	(IMG_INVALID, "IMG Invalid"),
+	(IMG_FORGED, "IMG Forged"),
+)
+
+NAME = 1
+DOB = 2
+PAN = 3
+IMG = 4
+
+FEEDBACK_CHOICES = (
+	(NAME, "Name Feedback"),
+	(DOB, "DOB Feedback"),
+	(PAN, "PAN Feedback"),
+	(IMG, "IMG Feedback")
+)
