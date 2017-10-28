03e9f488ea6af0f1ff6d0b2917e016e142928676
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-10-28 16:20:32 +0530

    Added changes in model

--- panverification/panapp/models.py
+++ panverification/panapp/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 from panapp.utils import get_upload_file_path
+from panapp.constants import INVALID_CHOICES, FEEDBACK_CHOICES
 
 # Create your models here.
 
@@ -22,16 +23,25 @@ class UserData(models.Model):
 	extracted_name = models.CharField(max_length=50, blank=True, null=True)
 	extracted_dob = models.CharField(max_length=12, blank=True, null=True)
 	extracted_pan = models.CharField(max_length=10, blank=True, null=True)
-	feedback_name = models.CharField(max_length=50, blank=True, null=True)
-	feedback_dob = models.CharField(max_length=12, blank=True, null=True)
-	feedback_pan = models.CharField(max_length=10, blank=True, null=True)
 	is_verified_auto = models.BooleanField(default=False)
 	is_verified_agent = models.BooleanField(default=False)
 	is_invalid_auto = models.BooleanField(default=False)
 	is_invalid_agent = models.BooleanField(default=False)
+	is_scanned = models.BooleanField(default=False)
 	user = models.ForeignKey(User, blank=True, null=True)
 	agent = models.ForeignKey(Agent, blank=True, null=True)
 
 	def __unicode__(self):
 		return self.user.username
+
+
+class FailedUserData(models.Model):
+	user_data = models.ForeignKey(UserData)
+	invalid = models.CharField(max_length=2, choices=INVALID_CHOICES)
+
+class FeedbackData(models.Model):
+	user_data = models.ForeignKey(UserData)
+	feedback_for = models.CharField(max_length=2, choices=FEEDBACK_CHOICES)
+	details = models.CharField(max_length=100)
+
 
