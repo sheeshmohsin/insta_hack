625aa3367632c81fb13b476f3008b631d4300328
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-10-28 23:37:22 +0530

    Deleted commented out code

--- panverification/panapp/models.py
+++ panverification/panapp/models.py
@@ -16,17 +16,11 @@ class Agent(models.Model):
 
 
 class UserData(models.Model):
-	# name = models.CharField(max_length=50)
-	# dob = models.CharField(max_length=12)
-	# pan = models.CharField(max_length=10)
 	image = models.FileField(upload_to=get_upload_file_path)
 	extracted_name = models.CharField(max_length=50, blank=True, null=True)
 	extracted_dob = models.CharField(max_length=12, blank=True, null=True)
 	extracted_pan = models.CharField(max_length=10, blank=True, null=True)
-	# is_verified_auto = models.BooleanField(default=False)
 	is_verified_agent = models.BooleanField(default=False)
-	# is_invalid_auto = models.BooleanField(default=False)
-	# is_invalid_agent = models.BooleanField(default=False)
 	is_scanned = models.BooleanField(default=False)
 	user = models.ForeignKey(User, blank=True, null=True)
 	agent = models.ForeignKey(Agent, blank=True, null=True)
