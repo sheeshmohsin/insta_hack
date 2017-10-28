406b17a3f38a2fa60c3f8b3d94e50d9b7deb3cd4
Sheesh Mohsin <sheeshmohsin@gmail.com>
2017-10-28 18:52:26 +0530

    Added tables in admin panel

--- panverification/panapp/admin.py
+++ panverification/panapp/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from panapp.models import UserData, FailedUserData, FeedbackData
 
 # Register your models here.
+admin.site.register(UserData)
+admin.site.register(FailedUserData)
+admin.site.register(FeedbackData)
